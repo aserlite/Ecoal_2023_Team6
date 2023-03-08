ab6e426bc6c96e5abc7c108e4424d36c67a7fb64
aserlite <arthurcuvillon691@gmail.com>
2023-03-08 11:38:33 +0000

    Search bar

--- client/src/Search/Search.js
+++ client/src/Search/Search.js
@@ -3,25 +3,24 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 function Search() {
-
+    const [search,setSearch]=useState("")
     const [article, setArticle] = useState([])
 
-    async function getArticles() {
-        const article = (await axios.post('http://localhost:8000/api/allarticle')).data
-        setArticle(article)
-        console.log(article)
-    }
-
     useEffect(() => {
-        getArticles()
-    }, []);
+        (async()=>await searchArticle())()
+    }, [search]);
 
+    async function searchArticle(){
+        const article = (await axios.post('http://localhost:8000/api/byTitle/'+search)).data
+        console.log("==",article)
+        setArticle(article)
+    }
     return (
         <>
             <label for="site-search">Search the site:</label>
-            <input type="search" id="site-search" name="searchbar"/>
+            <input type="search" id="site-search" name="searchbar" value={search} onChange={(e)=>setSearch(e.target.value)}/>
             <button>Search</button>
-            {article.map(a => <li > {a.title} <br/> <p dangerouslySetInnerHTML={{ __html: a.content }} /> </li >)}        
+            {article.map(a => <li > {a.title} <br/> <p dangerouslySetInnerHTML={{ __html: a.content }} /> </li >)}
         </>
         );
 }
