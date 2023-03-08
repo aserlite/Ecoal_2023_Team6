f4ef574bfb99649979fcac84aec5a56acf5e83af
aserlite <arthurcuvillon691@gmail.com>
2023-03-08 09:47:34 +0000

    Burger Patch

--- client/src/Slidebar/Slidebar.js
+++ client/src/Slidebar/Slidebar.js
@@ -7,12 +7,14 @@ function Slidebar() {
 
     return (
         <>
+            <div className="outer_container">
             <Menu right>
                 <Link to="/search" id="search"><a className="menu-item" href="/"> Search <i className='bx bx-search' style={{color: "red"}}></i></a></Link>
                 <Link to ="/home"><a id="home" className="menu-item" href="/home">Home</a></Link>
                 <Link to="/articles" id="about"><a className="menu-item" href="/articles">Articles</a></Link>
                 <Link to="/contact"><a id="contact" className="menu-item" href="/contact">Contact</a></Link>
             </Menu>
+            </div>
         </>
     );
 }
