98e0c76c3ff55efa4221c0c8f9fcaf957699532c
aserlite <arthurcuvillon691@gmail.com>
2023-03-09 10:54:17 +0000

    nav patch

--- client/src/Slidebar/Slidebar.js
+++ client/src/Slidebar/Slidebar.js
@@ -2,6 +2,8 @@ import './Slidebar.css'
 import { slide as Menu } from 'react-burger-menu'
 import { Link } from "react-router-dom"
 import {BsSearch} from "react-icons/bs";
+import { AiOutlineUserAdd } from "react-icons/ai";
+
 
 
 function Slidebar() {
@@ -10,11 +12,16 @@ function Slidebar() {
         <>
             <div className="outer_container">
             <Menu right>
-                <Link to="/search" id="search"><a className="menu-item" href="/"> <BsSearch /></a></Link>
+                <div className="icons_bar">
+                    <Link to="/login" id="login"><a className="menu-item login_menu_item" href="/login"><AiOutlineUserAdd /> </a></Link>
+
+                    <Link to="/search" id="search"><a className="menu-item" href="/"> <BsSearch /></a></Link>
+
+                </div>
                 <Link to ="/"><a id="home" className="menu-item" href="/home">Home</a></Link>
-                <Link to="/articles" id="about"><a className="menu-item" href="/articles">Articles</a></Link>
-                <Link to="/login" id="login"><a className="menu-item" href="/login">Login</a></Link>
-                <Link to="/register" id="register"><a className="menu-item" href="/register">Register</a></Link>
+                <Link to="/articles" id="articles"><a className="menu-item" href="/articles">Articles</a></Link>
+                <Link to="/latest" id="latest"><a className="menu-item" href="/latest">Latests Posts</a></Link>
+                <Link to="/about" id="about"><a className="menu-item" href="/about">About Us</a></Link>
             </Menu>
             </div>
         </>
