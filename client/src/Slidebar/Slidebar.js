653473cd4c4a129215eae72cce41d440ff6413c1
aserlite <arthurcuvillon691@gmail.com>
2023-03-09 08:40:15 +0000

    link patch

--- client/src/Slidebar/Slidebar.js
+++ client/src/Slidebar/Slidebar.js
@@ -13,7 +13,7 @@ function Slidebar() {
                 <Link to="/search" id="search"><a className="menu-item" href="/"> <BsSearch /></a></Link>
                 <Link to ="/"><a id="home" className="menu-item" href="/home">Home</a></Link>
                 <Link to="/articles" id="about"><a className="menu-item" href="/articles">Articles</a></Link>
-                <Link to="/login" id="about"><a className="menu-item" href="/login">Login</a></Link>
+                <Link to="/login" id="login"><a className="menu-item" href="/login">Login</a></Link>
             </Menu>
             </div>
         </>
