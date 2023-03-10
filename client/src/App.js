66cb13e73d7d7555d0cc8a9c7c3f3436fee26ac0
honorine <honorineru@gmail.com>
2023-03-10 10:37:42 +0100

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -10,15 +10,17 @@ import Register from "./Register/Register";
 import Add from "./Add/Add";
 import Article from "./Article/Article";
 import About from "./About/About";
-import {useCookies} from 'react-cookie';
+import { useCookies } from 'react-cookie';
+
+
 
 function App() {
 
-    const [cookies, setCookie, removeCookie] = useCookies(['mycookie']);
+  const [cookies, setCookie, removeCookie] = useCookies(['mycookie']);
 
   return (
     <>
-        <Slidebar/>
+      <Slidebar />
 
       <nav>
         <Link to="/" className='home'><img className="logo" src={logo} alt="Logo" /></Link>
@@ -27,17 +29,18 @@ function App() {
 
 
       <Routes>
-        <Route exact={true} path="/" element={<Home/>} />
+        <Route exact={true} path="/" element={<Home />} />
         <Route exact={true} path="/articles" element={<Articles />} />
         <Route exact={true} path="/article/:id" element={<Article />} />
         <Route exact={true} path="/search" element={<Search />} />
-        <Route exact={true} path="/login" element={<Login setCookie={setCookie}/>} />
+        <Route exact={true} path="/login" element={<Login setCookie={setCookie} />} />
         <Route exact={true} path="/register" element={<Register setCookie={setCookie} />} />
         <Route exact={true} path="/add" element={<Add cookies={cookies} />} />
         <Route exact={true} path="/about" element={<About />} />
 
         <Route path="*" element={() => <p>Page Not Found</p>} />
       </Routes>
+
     </>
   );
 }
